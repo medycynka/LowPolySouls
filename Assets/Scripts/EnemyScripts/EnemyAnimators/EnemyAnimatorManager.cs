ec41b39379d64a6be80c27eda89d4b64e9906820
medycynka <tyrranozaur08@gmail.com>
2020-12-14 15:54:41 +0100

    added recorer

--- Assets/Scripts/EnemyScripts/EnemyAnimators/EnemyAnimatorManager.cs
+++ Assets/Scripts/EnemyScripts/EnemyAnimators/EnemyAnimatorManager.cs
@@ -20,8 +20,16 @@ namespace SP
             enemyLocomotionManager.enemyRigidBody.drag = 0;
             Vector3 deltaPosition = anim.deltaPosition;
             deltaPosition.y = 0;
-            Vector3 velocity = deltaPosition / delta;
-            enemyLocomotionManager.enemyRigidBody.velocity = velocity;
+            
+            if (delta != 0.0f)
+            {
+                Vector3 velocity = deltaPosition / delta;
+                enemyLocomotionManager.enemyRigidBody.velocity = velocity;
+            }
+            else
+            {
+                enemyLocomotionManager.enemyRigidBody.velocity = Vector3.zero;
+            }
         }
     }
 
