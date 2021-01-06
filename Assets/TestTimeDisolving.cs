7a92b14bf7bf538ad0ef96b618811bb1b4467d62
medycynka <tyrranozaur08@gmail.com>
2021-01-07 00:34:13 +0100

    finished disolve shader

---
+++ Assets/TestTimeDisolving.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTimeDisolving : MonoBehaviour
+{
+    public Material characterMaterial;
+    public bool shouldDisolve = false;
+    public float currentTime_ = 0.0f;
+    public float endTime_ = 2.0f;
+    bool reset = false;
+
+    void Start()
+    {
+        characterMaterial = GetComponentInChildren<Renderer>().material;
+    }
+
+    void Update()
+    {
+        if (shouldDisolve)
+        {
+            reset = true;
+            currentTime_ += Time.deltaTime;
+            characterMaterial.SetFloat("_DisolveValue", Mathf.Lerp(0.0f, 1.0f, currentTime_ / endTime_));
+
+            if(currentTime_ >= endTime_)
+            {
+                shouldDisolve = false;
+            }
+        }
+        else if (reset)
+        {
+            reset = false;
+            currentTime_ = 0.0f;
+            characterMaterial.SetFloat("_DisolveValue", 0.0f);
+        }
+    }
+}
